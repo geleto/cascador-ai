89a95fee2c2ff25c1e631190dd49f7973a7db62b
Angel Popov <geleto@gmail.com>
2025-01-10 00:48:06 +0000

    Add vercel config argument types

--- src/types.ts
+++ src/types.ts
@@ -4,7 +4,6 @@ import { ILoaderPAsync, ILoaderAny } from 'cascada-tmpl';
 import { Config } from './Config';
 
 export type Context = Record<string, any>;
-
 export type Filters = Record<string, (input: any, ...args: any[]) => Promise<any> | any>;
 
 export interface TemplateConfig {
@@ -16,15 +15,17 @@ export interface TemplateConfig {
 	prompt?: string;
 }
 
-// Generator configurations - extend TemplateConfig with respective tool configs
-export type TextGeneratorConfig = Parameters<typeof generateText>[0] & TemplateConfig;
-export type TextStreamerConfig = Parameters<typeof streamText>[0] & TemplateConfig;
-export type ObjectGeneratorConfig = Parameters<typeof generateObject>[0] & TemplateConfig;
-export type ObjectStreamerConfig = Parameters<typeof streamObject>[0] & TemplateConfig;
+// Vercel AI SDK configurations
+export type GenerateTextConfig = Parameters<typeof generateText>[0];
+export type StreamTextConfig = Parameters<typeof streamText>[0];
+export type GenerateObjectConfig = Parameters<typeof generateObject>[0];
+export type StreamObjectConfig = Parameters<typeof streamObject>[0];
 
-// Make all properties optional at the CommonConfig level
-export type CommonConfig = Partial<TextGeneratorConfig> |
-	Partial<TextStreamerConfig> |
-	Partial<ObjectGeneratorConfig> |
-	Partial<ObjectStreamerConfig> |
-	TemplateConfig;
+// Generator configurations - extend TemplateConfig with respective tool configs
+export type TextGeneratorConfig = GenerateTextConfig & TemplateConfig;
+export type TextStreamerConfig = StreamTextConfig & TemplateConfig;
+export type ObjectGeneratorConfig = GenerateObjectConfig & TemplateConfig;
+export type ObjectStreamerConfig = StreamObjectConfig & TemplateConfig;
+
+// Combined configuration type that can be any of the generator configs
+export type CommonConfig = TextGeneratorConfig | TextStreamerConfig | ObjectGeneratorConfig | ObjectStreamerConfig | TemplateConfig;
