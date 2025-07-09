627deb4a56f4b5827ed3092d418b4797ad1136e3
Angel Popov <geleto@gmail.com>
2025-07-09 09:40:21 +0100

    Add test infrastructure

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,21 @@
-import { expect } from 'chai';
+import { expect, testUtils, createTestConfig } from './setup.js';
 
 describe('Example Test', () => {
 	it('should work', () => {
 		expect(true).to.be.true;
+	});
+
+	it('should load environment variables', () => {
+		// Test that environment variables can be accessed
+		const config = createTestConfig();
+		expect(config.nodeEnv).to.equal('test');
+		expect(config.apiKey).to.be.a('string');
+	});
+
+	it('should have test utilities available', () => {
+		// Test that the utility functions work correctly
+		expect(testUtils.isEnvLoaded('NODE_ENV')).to.be.a('boolean');
+		expect(testUtils.getEnv('NODE_ENV', 'test')).to.equal('test');
+		expect(testUtils.getEnv('NON_EXISTENT_VAR', 'default')).to.equal('default');
 	});
 });
