675a9949fa60fce18fd0cc31fcaa2323282dcb48
Angel Popov <geleto@gmail.com>
2025-07-14 12:21:18 +0100

    Export all types

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,33 @@
+export { TemplateRenderer, TemplateRendererInstance } from './factory-template';
+export { ScriptRunner, ScriptRunnerInstance } from './factory-script';
+export { TextGenerator, TextStreamer, TextGeneratorInstance, TextGeneratorConfig } from './factory-text';
+export { ObjectGenerator, ObjectStreamer, ObjectGeneratorInstance, ObjectGeneratorConfig } from './factory-object';
+export { Tool } from './factory-tool';
+export { Config } from './factory-config';
+
+export { ConfigProvider, ConfigData } from './ConfigData';
+
+export {
+	TemplateConfig, ScriptConfig,
+	GenerateTextConfig, StreamTextConfig,
+	GenerateObjectObjectConfig, GenerateObjectArrayConfig, GenerateObjectEnumConfig, GenerateObjectNoSchemaConfig,
+	StreamObjectObjectConfig, StreamObjectArrayConfig, StreamObjectNoSchemaConfig,
+	ToolConfig
+} from './types-config';
+
+// Core types
+export { Context, SchemaType, TemplatePromptType, ScriptType, LLMPromptType, PromptOrMessage } from './types';
+
+// Result types
+export {
+	ScriptResult,
+	GenerateObjectResultAll, GenerateObjectObjectResult, GenerateObjectArrayResult, GenerateObjectEnumResult, GenerateObjectNoSchemaResult,
+	StreamObjectResultAll, StreamObjectObjectResult, StreamObjectArrayResult, StreamObjectNoSchemaResult
+} from './types-result';
+
+// Type utilities
+// export * from './type-utils';
+
+//export the factory create namespace
 import * as factories from './factories';
-
-import { Config } from './factory-config';
-import { TemplateRenderer } from './factory-template';
-import { ScriptRunner } from './factory-script';
-import { TextGenerator, TextStreamer } from './factory-text';
-import { ObjectGenerator, ObjectStreamer } from './factory-object';
-import { Tool } from './factory-tool';
-
-export { Config, TemplateRenderer, ScriptRunner, TextGenerator, TextStreamer, ObjectGenerator, ObjectStreamer, Tool };
-
 export const create = factories;
-export * from './factories';
